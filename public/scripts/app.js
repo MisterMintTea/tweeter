6ea9376cbdff82df84d94554d38491e825f679dd
Victor Huynh <victor.h.huynh@gmail.com>
2019-05-17 18:58:09 +0000

    adding mongo db

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,112 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
+  const createTweetSection = tweetObj => {
+      const $article = $('<article>').addClass('.tweet-container');
+
+      const $header = $('<header>').addClass('.tweet-header');
+
+      const $h1TweetName = $('<h1>').text(tweetObj.user.name);
+
+      const $avatars = $('<img>').attr('src', tweetObj.user.avatars.small);
+      
+      const $h2Handler = $('<h2>').text(tweetObj.user.handle);
+      
+      $header.append($h1TweetName);
+      $h1TweetName.append($avatars);
+      $avatars.append($h2Handler);
+
+      const $content = $('<h3>').text(tweetObj.content.text);
+
+      const $footer = $('<footer>');
+
+      const $date = $('<h4>').text(new Date(tweetObj.created_at));
+
+      $footer.append($date);
+
+      $article.append($header);
+      $article.append($content);
+      $article.append($footer);
+
+    console.log($article);
+      return $article;
+  }
+  
+const renderTweets = postArr => {
+  for (const postEl of postArr) {
+    const post = createTweetSection(postEl);
+    $('#old-tweet').prepend(post);
+  }
+}
+
+// Loads db 
+const loadTweets = (url) => {
+  $.ajax({
+    method: 'GET',
+    url: url
+  })
+
+  .done(response => {
+    renderTweets(response);
+  })
+
+  .fail(error => {
+    console.log(`Error: ${error}`);
+  })
+  .always(() => {
+    console.log('Request completed.');
+  })
+}
+
+function postTweets(url, pData){
+  $.ajax({
+    method: 'POST',
+    url: url,
+    data: pData
+  })
+  .done(response => {
+    $('#old-tweet').empty()
+    loadTweets(url)
+    
+  })
+  .fail(error => {
+    console.log(`Error: ${error}`);
+  })
+  .always(() => {
+    console.log('Request completed.');
+  })
+}
+
+
+  $(document).ready(function() {
+    loadTweets('http://localhost:8080/tweets');
+
+    $("#btn-compose").click(function() {
+      $("#new-tweet").slideToggle("slow");
+      $("#text-area").focus();
+      });
+    
+   
+    $( "form" ).on( "submit", function( event ) {
+      event.preventDefault();
+      const data = $(this).serialize();
+      console.log($(this).serialize());
+
+      if (data.length === 5) {
+        $("#error").slideDown("slow");
+        return;
+      }if (data.length > 145) {
+        $("#error").slideDown("slow");
+        return;
+      } else {
+        $("#error").hide();
+        postTweets('http://localhost:8080/tweets', data)
+        return;
+      }
+    })
+  })
+  
+
+// (.counter).html(140);
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('.new-tweet form textarea').on('keydown', function (event){
+
+    const maxChar = 140;
+
+    const $currentChar = $(this).val().length;
+
+    const charRemaining = maxChar - $currentChar;
+
+    let $counterDisplay = $(this).parent().find('span');
+
+    $counterDisplay.text(charRemaining);
+
+    charRemaining < 0 ? $counterDisplay.addClass('red') : $counterDisplay.removeClass('red');
+
+    });
+  });
