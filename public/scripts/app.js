1305d6ae5ab4081a1ef9506612a4b8bd390d6481
Victor Huynh <victor.h.huynh@gmail.com>
2019-05-20 07:52:54 +0000

    May 20th submission

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,112 +4,133 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+ // Function that creates new html elements
+const createTweetSection = tweetObj => {
+  const $article = $("<article>").addClass("tweet-container");
 
+  const $header = $("<header>").addClass("tweet-header");
 
-  const createTweetSection = tweetObj => {
-      const $article = $('<article>').addClass('.tweet-container');
+  const $h2TweetName = $("<h2>").text(tweetObj.user.name);
 
-      const $header = $('<header>').addClass('.tweet-header');
+  const $avatars = $("<img>").attr("src", tweetObj.user.avatars.small).addClass("avatar");
 
-      const $h1TweetName = $('<h1>').text(tweetObj.user.name);
+  const $h4Handler = $("<h4>").text(tweetObj.user.handle).addClass("handler");
 
-      const $avatars = $('<img>').attr('src', tweetObj.user.avatars.small);
-      
-      const $h2Handler = $('<h2>').text(tweetObj.user.handle);
-      
-      $header.append($h1TweetName);
-      $h1TweetName.append($avatars);
-      $avatars.append($h2Handler);
-
-      const $content = $('<h3>').text(tweetObj.content.text);
-
-      const $footer = $('<footer>');
-
-      const $date = $('<h4>').text(new Date(tweetObj.created_at));
-
-      $footer.append($date);
-
-      $article.append($header);
-      $article.append($content);
-      $article.append($footer);
-
-    console.log($article);
-      return $article;
-  }
+  $header.append($avatars);
+  $header.append($h2TweetName);
+  $header.append($h4Handler);
   
+
+  const $content = $("<h3>").text(tweetObj.content.text);
+
+  const $footer = $("<footer>").addClass("footer");
+
+  const $iconFlag = $("<i>").addClass("fab fa-font-awesome-flag");
+  const $iconHeart = $("<i>").addClass("fas fa-heart");
+  const $iconRetweet = $("<i>").addClass("fas fa-retweet");
+
+  const $date = $("<h4>").text(moment(new Date(tweetObj.created_at)).fromNow());
+
+  $footer.append($date);
+  $footer.append($iconFlag);
+  $footer.append($iconHeart);
+  $footer.append($iconRetweet);
+  
+  $article.append($header);
+  $article.append($content);
+  $article.append($footer);
+
+  return $article;
+};
+
+// Creates new tweets in the database
 const renderTweets = postArr => {
   for (const postEl of postArr) {
     const post = createTweetSection(postEl);
-    $('#old-tweet').prepend(post);
+    $("#old-tweet").prepend(post);
   }
-}
+};
 
-// Loads db 
-const loadTweets = (url) => {
+// Gets tweets from Database
+const loadTweets = url => {
   $.ajax({
-    method: 'GET',
+    method: "GET",
     url: url
   })
 
-  .done(response => {
-    renderTweets(response);
-  })
+    .done(response => {
+      renderTweets(response);
+    })
 
-  .fail(error => {
-    console.log(`Error: ${error}`);
-  })
-  .always(() => {
-    console.log('Request completed.');
-  })
-}
+    .fail(error => {
+      console.log(`Error: ${error}`);
+    })
+    .always(() => {
+      console.log("Request completed.");
+    });
+};
 
-function postTweets(url, pData){
+// Adds tweets to Database
+function postTweets(url, pData) {
   $.ajax({
-    method: 'POST',
+    method: "POST",
     url: url,
     data: pData
   })
-  .done(response => {
-    $('#old-tweet').empty()
-    loadTweets(url)
-    
-  })
-  .fail(error => {
-    console.log(`Error: ${error}`);
-  })
-  .always(() => {
-    console.log('Request completed.');
-  })
+    .done(response => {
+      $("#old-tweet").empty();
+      loadTweets(url);
+      $('.counter').text(140);
+      $('form')[0].reset();
+    })
+    .fail(error => {
+      console.log(`Error: ${error}`);
+    })
+    .always(() => {
+      console.log("Request completed.");
+    });
 }
 
+// Loads primary functionality as the page is loaded
+$(document).ready(function() {
+  loadTweets("http://localhost:8080/tweets");
 
-  $(document).ready(function() {
-    loadTweets('http://localhost:8080/tweets');
+  $("#btn-compose").click(function() {
+    $("#new-tweet").slideToggle("slow");
+    $("#text-area").focus();
+  });
 
-    $("#btn-compose").click(function() {
-      $("#new-tweet").slideToggle("slow");
-      $("#text-area").focus();
-      });
-    
-   
-    $( "form" ).on( "submit", function( event ) {
-      event.preventDefault();
-      const data = $(this).serialize();
-      console.log($(this).serialize());
+  $("#new-tweet").hide();
+  $("#b-navbar-fg").hide();
 
-      if (data.length === 5) {
-        $("#error").slideDown("slow");
-        return;
-      }if (data.length > 145) {
-        $("#error").slideDown("slow");
-        return;
-      } else {
-        $("#error").hide();
-        postTweets('http://localhost:8080/tweets', data)
-        return;
-      }
-    })
-  })
-  
+  $("#handler").hover(
+    function () {
+      $(this).show();
+    },
+    function () {
+      $(this).hide();
+    }
+  );
 
-// (.counter).html(140);
+  // Displays appropriate errors for textarea
+  $(".new-tweet form").on("submit", function(event) {
+    event.preventDefault();
+    const data = $(this).serialize();
+    const input = $(this)
+      .find("textarea")
+      .val();
+
+    if (input.length === 0) {
+      $("#error").slideDown("slow").text("The tweet is empty!");
+      return;
+    }
+    if (input.length > 140) {
+      $("#error").slideDown("slow").text("Too many characters!");
+      return;
+    } else {
+      $("#error").hide();
+      postTweets("http://localhost:8080/tweets", data);
+      return;
+    }
+  });
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
-    $('.new-tweet form textarea').on('keydown', function (event){
-
+  $(".new-tweet form textarea").on("input", function(event) {
     const maxChar = 140;
 
     const $currentChar = $(this).val().length;
 
     const charRemaining = maxChar - $currentChar;
 
-    let $counterDisplay = $(this).parent().find('span');
+    let $counterDisplay = $(this)
+      .parent()
+      .find("span");
 
     $counterDisplay.text(charRemaining);
 
-    charRemaining < 0 ? $counterDisplay.addClass('red') : $counterDisplay.removeClass('red');
-
-    });
+    charRemaining < 0
+      ? $counterDisplay.addClass("red")
+      : $counterDisplay.removeClass("red");
   });
+});
